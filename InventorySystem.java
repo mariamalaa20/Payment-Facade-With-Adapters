ac6c7434f140fc03a76eaa7549f7aa775ba13d81
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-03-25 06:09:45 +0300

    Add files via upload

---
+++ InventorySystem.java
@@ -0,0 +1,9 @@
+public class InventorySystem {
+    public boolean isAvailable(String productId) {
+        return true; // simulate availability
+    }
+
+    public void reduceStock(String productId) {
+        System.out.println("Stock reduced for product: " + productId);
+    }
+}
