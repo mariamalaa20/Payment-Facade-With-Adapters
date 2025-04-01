ab0ed2d65d874f904d3d50bc2ea36a62471f4b64
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-04-01 05:57:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        OrderManager manager = new OrderManager();
+        manager.processOrder("ABC123", "123 Main Street");
+
+        PaymentMethod payment = new CreditCardAdapter();
+        payment.makePayment(100.0);
+    }
+}
 
